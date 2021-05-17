0ca02acf636ce8db428edce312726167be23f61b
William Beinö <william.beino@hotmail.com>
2021-05-17 23:45:03 +0200

    cleaner open set loop

--- path_finding.cpp
+++ path_finding.cpp
@@ -1,5 +1,5 @@
 #include <cmath>
-#include <queue>
+#include <list>
 #include <vector>
 #include <iostream>
 using namespace std;
@@ -11,12 +11,23 @@ struct sNode {
 	bool bVisited = false;
 	bool bTraversable = false;
 
-	float fCost = INFINITY;
-	float fDistance = INFINITY;
+	float fCost = INFINITY;		// Cost to walk to, from start
+	float fDistance = INFINITY; // Distance to end
 
-	sNode* previous = nullptr;
 	vector<sNode*> vNeighbors;
+	sNode* previous = nullptr;
 };
+
+vector<int> RetracePath(sNode* p, int nMapWidth) {
+	vector<int> cords;
+
+	while (p->previous != nullptr) {
+		int index = p->y * nMapWidth + p->x;
+		cords.push_back(index);
+		p = p->previous;
+	}
+	return cords;
+}
 
 // Implementation of A* path finding
 int FindPath(const int nStartX, const int nStartY,
@@ -32,24 +43,24 @@ int FindPath(const int nStartX, const int nStartY,
 	//Initialize nodes
 	for (int i = 0; i < nMapSize; i++) {
 		nodes[i].bTraversable = pMap[i] == 1;
-		nodes[i].x = i % nMapWidth;
-		nodes[i].y = i / (nMapHeight+1);
-	}
 
-	//Initialize nodes' neighbors
-	for (int i = 0; i < nMapSize; i++) {
-		sNode* node = &nodes[i];
-		int x = node->x;
-		int y = node->y;
+		// Calculate positions
+		int x = i % nMapWidth;
+		int y = i / (nMapHeight + 1);
 
+
+		nodes[i].x = x;
+		nodes[i].y = y;
+
+		// Add neighbors
 		if (y > 0)
-			node->vNeighbors.push_back(&nodes[(y - 1) * nMapWidth + x]);
+			nodes[i].vNeighbors.push_back(&nodes[(y - 1) * nMapWidth + x]);
 		if (y < nMapHeight - 1)
-			node->vNeighbors.push_back(&nodes[(y + 1) * nMapWidth + x]);
+			nodes[i].vNeighbors.push_back(&nodes[(y + 1) * nMapWidth + x]);
 		if (x > 0)
-			node->vNeighbors.push_back(&nodes[y * nMapWidth + (x - 1)]);
+			nodes[i].vNeighbors.push_back(&nodes[y * nMapWidth + (x - 1)]);
 		if (x < nMapWidth - 1)
-			node->vNeighbors.push_back(&nodes[y * nMapWidth + (x + 1)]);
+			nodes[i].vNeighbors.push_back(&nodes[y * nMapWidth + (x + 1)]);
 	}
 
 	// Using manhattan distance as the heuristic function
@@ -57,56 +68,44 @@ int FindPath(const int nStartX, const int nStartY,
 		return (abs(a->x - b->x) + abs(a->y - b->y));
 	};
 
+
 	//Setup starting node and adding it to the open set.
 	sNode* current = nodeStart;
 	current->fCost = 0.0f;
 	current->fDistance = heuristic(nodeStart, nodeEnd);
-	priority_queue<sNode*> pqOpenSet;
-	pqOpenSet.push(current);
+
+	list<sNode*> pqOpenSet;
+	pqOpenSet.push_back(current);
 
 	//Open set loop
-	while (! pqOpenSet.empty() && current != nodeEnd) {
-
-		while (! pqOpenSet.empty() && pqOpenSet.top()->bVisited)
-			pqOpenSet.pop();
-
-		if (pqOpenSet.empty())
-			break;
-
-		current = pqOpenSet.top();
+	while (! pqOpenSet.empty()) {
+		current = pqOpenSet.front();
 		current->bVisited = true;
+		pqOpenSet.pop_front();
+
+		// Found path
+		if (current == nodeEnd) {
+			vector<int> cords = RetracePath(nodeEnd, nMapWidth);
+			if (cords.size() < nOutBufferSize) { //Output path to pOutBuffer if possible.
+				reverse(cords.begin(), cords.end());
+				copy(cords.begin(), cords.end(), pOutBuffer);
+			}
+			return cords.size();
+		}
 
 		for (auto neighbor : current->vNeighbors) {
-			if (!neighbor->bVisited && neighbor->bTraversable) {
-				pqOpenSet.push(neighbor);
+			if (! neighbor->bTraversable || neighbor->bVisited) {
+				continue;
 			}
-
+			
 			float tentativeCost = current->fCost + heuristic(current, neighbor);
-
-			if (tentativeCost < neighbor->fCost) {
-				neighbor->previous = current;
+			if (tentativeCost < neighbor->fCost || ! neighbor->bVisited) {
 				neighbor->fCost = tentativeCost;
-				neighbor->fDistance = neighbor->fCost + heuristic(neighbor, nodeEnd);
+				neighbor->previous = current;
+				if (!neighbor->bVisited)
+					pqOpenSet.push_back(neighbor);
 			}
 		}
-	}
-
-	// Calculate and return distance from path
-	if (current == nodeEnd) {
-		sNode* p = nodeEnd;
-		vector<int> cords;
-
-		while (p->previous != nullptr) {
-			int index = p->y * nMapWidth + p->x;
-			cords.push_back(index);
-			p = p->previous;
-		}
-
-		if (cords.size() < nOutBufferSize) { //Output path to pOutBuffer if possible.
-			reverse(cords.begin(), cords.end());
-			copy(cords.begin(), cords.end(), pOutBuffer);
-		}
-		return cords.size();
 	}
 	// Couldn't find a path.
 	return -1;
